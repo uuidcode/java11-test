db5441b188feb758d772d8bf2d7794201692971a
ted.song <ted.song@daumkakao.com>
2018-11-07 01:24:35 +0900

    render

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -1,17 +1,29 @@
 var java11 = {
     init: function () {
         $('.load-button').on('click', function () {
-            var source = jQuery('#server-hbs').html();
-            var template = Handlebars.compile(source);
-
-            $.ajax({
-                url: '/api',
-                dataType: 'json',
-                success: function (json) {
-                    var html = template(json);
-                    $('#content').html(html);
-                }
+            java11.render({
+                templateId: 'server-hbs',
+                api: '/api',
+                contentId: 'content'
             });
+        });
+    },
+
+    render: function (option) {
+        var templateId = option.templateId;
+        var api = option.api;
+        var contentId = option.contentId;
+
+        var source = jQuery('#' + templateId).html();
+        var template = Handlebars.compile(source);
+
+        $.ajax({
+            url: api,
+            dataType: 'json',
+            success: function (json) {
+                var html = template(json);
+                $('#' + contentId).html(html);
+            }
         });
     }
 };
