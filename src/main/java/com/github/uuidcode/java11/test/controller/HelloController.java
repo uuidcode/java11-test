bb02d08ea4c6e4f2a0b22728e75d6d7e50b9c338
uuidcode <uuidcode@gmail.com>
2018-11-06 13:51:40 +0900

    WebFluxTest

--- src/main/java/com/github/uuidcode/java11/test/controller/HelloController.java
+++ src/main/java/com/github/uuidcode/java11/test/controller/HelloController.java
@@ -5,10 +5,14 @@ import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.github.uuidcode.java11.test.model.Project;
+
+import reactor.core.publisher.Mono;
 
 import static com.github.uuidcode.java11.test.util.CoreUtil.createUUID;
 
@@ -33,5 +37,18 @@ public class HelloController {
             .collect(Collectors.toList());
 
         model.addAttribute("projectList", projectList);
+    }
+
+    @ResponseBody
+    @GetMapping("/api")
+    public Mono<String> helloApi() {
+        WebClient client = WebClient.builder().build();
+
+        return client.get()
+            .uri("https://jsonplaceholder.typicode.com/todos/1")
+            .retrieve()
+            .bodyToMono(String.class)
+            .map(body -> body.toUpperCase())
+            .switchIfEmpty(Mono.just("Empty"));
     }
 }
--- src/test/java/com/github/uuidcode/java11/test/controller/WebFluxTest.java
+++ src/test/java/com/github/uuidcode/java11/test/controller/WebFluxTest.java
@@ -2,18 +2,51 @@ package com.github.uuidcode.java11.test.controller;
 
 import org.junit.Test;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebFluxTest {
     @Test
-    public void mono() {
+    public void mono1() {
         Mono<Integer> mono = Mono.just(1)
             .subscribeOn(Schedulers.single());
 
         mono.subscribe(item -> assertThat(item).isEqualTo(1));
+    }
 
+    @Test
+    public void mono2() {
+        Mono<Integer> mono = Mono.just(1)
+            .subscribeOn(Schedulers.single());
+
+        assertThat(mono.block()).isEqualTo(1);
+    }
+
+    @Test
+    public void mono3() {
+        Mono<Integer> mono = Mono.just(1)
+            .subscribeOn(Schedulers.single());
+
+        StepVerifier.create(mono)
+            .expectNext(1)
+            .verifyComplete();
+    }
+
+    @Test
+    public void flux1() {
+        var flux = Flux.just(1, 2, 3)
+            .concatWith(Mono.error(new RuntimeException()))
+            .subscribeOn(Schedulers.single());
+
+        StepVerifier.create(flux)
+            .expectNext(1)
+            .expectNext(2)
+            .expectNext(3)
+            .expectError(RuntimeException.class)
+            .verify();
     }
 }
